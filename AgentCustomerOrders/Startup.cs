45edeea26e0ca2bdd7a5c69944d8b177a8c8ea4a
retsullivan <retsullivan@gmail.com>
2021-05-31 16:24:06 -0500

    connecting macbook to the repo

--- AgentCustomerOrders/Startup.cs
+++ AgentCustomerOrders/Startup.cs
@@ -48,6 +48,9 @@ namespace AgentCustomerOrders
 
             app.UseEndpoints(endpoints =>
             {
+                //endpoints.MapControllerRoute(
+                //    name: "AddAgent",
+                //    pattern:"Agent/addAgent");
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
